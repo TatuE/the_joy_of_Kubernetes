48aa07b1ebaec70e0d6737f974f87999ba7cef40
Tanssiva kuu <22812243+TatuE@users.noreply.github.com>
2025-05-01 18:22:59 +0300

    updated installation script

--- Scripts/install_K3s_server.sh
+++ Scripts/install_K3s_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CONTROL_PLANE_IFACE="enp7s0"
-CONTROL_PLANE_PRIVATE_IP=$(ip -4 addr show ${CONTROL_PLANE_IFACE} | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+CONTROL_PLANE_IFACE="enp7s0" # Add the interface to the Kubernetes network ( Note, that in our case, this is a private network)
+CONTROL_PLANE_PRIVATE_IP=$(ip -4 addr show ${CONTROL_PLANE_IFACE} | grep -oP '(?<=inet\s)\d+(\.\d+){3}') # Get the IPV address from the interface.
 
 # Check if IP was found on the interface
 if [ -z "${CONTROL_PLANE_PRIVATE_IP}" ]; then
@@ -12,7 +12,32 @@ fi
 echo "Using IP ${CONTROL_PLANE_PRIVATE_IP} for interface ${CONTROL_PLANE_IFACE}"
 
 # Install K3s
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server \
-  --flannel-iface=${CONTROL_PLANE_IFACE} \
-  --node-ip=${CONTROL_PLANE_PRIVATE_IP} \
-  --node-external-ip=$(curl -s ifconfig.me)" sh -s -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --flannel-iface=${CONTROL_PLANE_IFACE} --node-ip=${CONTROL_PLANE_PRIVATE_IP} --node-external-ip=$(curl -s -4 ifconfig.me)" sh -s -
+
+# Installation script
+
+# curl -sfL https://get.k3s.io
+# This downloads the official K3s installation script from the internet
+# Curl flags
+#   - silent (-s)
+#   - fail silently on server errors (-f)
+#   - follow redirects (-L)
+#
+# "|"
+# Piping
+#
+# INSTALL_K3S_EXEC
+# Environment variable for K3s installation script downloaded
+#   - Server
+#     - Installs K3s in server (control-plane) mode
+#
+#   Flags
+#   --flannel-iface
+#     - Tells Flannel (the default CNI(Container Network Interface)) what network interface to use for cluster networking.
+#
+#   --node-ip
+#     - Sets the internal IP address K3s advertises for this node
+#
+#   --node-external-ip=
+#     - Sets the external IP address K3s advertises
+#     - "curl -4 ifconfig.me" fetches the public IP address of the server. -4 flag specifies IPV4 type address
