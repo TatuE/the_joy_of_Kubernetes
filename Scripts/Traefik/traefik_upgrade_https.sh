6c020d50a72b7b7241aabc003f88ff5aa2e13171
tanssiva_kuu <22812243+TatuE@users.noreply.github.com>
2025-05-14 17:42:02 +0300

    Update traefik_upgrade_https.sh

--- Scripts/Traefik/traefik_upgrade_https.sh
+++ Scripts/Traefik/traefik_upgrade_https.sh
@@ -3,7 +3,7 @@
 
 helm upgrade traefik traefik/traefik \
    --namespace traefik \
-   --reuse-values \ # Important: This keeps your existing Helm values (like service.type=LoadBalancer and annotations)
+   --reuse-values \ # Important: This keeps the existing Helm values (like service.type=LoadBalancer and annotations)
    --set "additionalArguments={--certificatesresolvers.myresolver.acme.email=it-support@erkinjuntti.eu,--certificatesresolvers.myresolver.acme.storage=/data/acme.json,--certificatesresolvers.myresolver.acme.httpchallenge.entrypoint=web}" \
    --set "ports.websecure.tls.enabled=true" \
    --set "entryPoints.web.http.redirections.entryPoint.to=websecure" \
